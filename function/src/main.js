7ad08015cf913851ba39e4e6d9387904039fef7d
Chris Coombs <chriscoombs@users.noreply.github.com>
2019-02-13 18:33:06 +1100

    Run task only for CI/CD builds

--- function/src/main.js
+++ function/src/main.js
@@ -42,33 +42,36 @@ const run = async () => {
     const sourcesDirectory = task.getVariable('Build.SourcesDirectory');
     const buildId = parseInt(task.getVariable('Build.BuildId'), 10);
     const project = task.getVariable('System.TeamProject');
-    console.log('Connecting to Build API...');
-    const buildApi = await getConnection().getBuildApi();
-    console.log('Getting build...');
-    const build = await buildApi.getBuild(buildId, project);
-    if (build.status === buildInterface.BuildStatus.NotStarted
-      || build.status === buildInterface.BuildStatus.InProgress) {
-      console.log(`Build ${buildInterface.BuildStatus.NotStarted ? 'not started' : 'in progress'}...`);
-      const options = {
-        cwd: sourcesDirectory,
-      };
-      console.log('Getting log for branch...');
-      const result = task.execSync('git', `diff-tree --no-commit-id --name-only -r ${commitId}`, options);
-      // check for errors
-      const files = result.stdout.toString().split('\n').filter(file => (file !== ''));
-      console.log('Getting definition for build...');
-      const definition = await buildApi.getDefinition(build.definition.id, project);
-      if (isInPathFilters(files, definition)) {
-        console.log('Files match! Continuing with build...');
-      } else {
-        console.log('Cancelling build...');
-        build.status = buildInterface.BuildStatus.Cancelling;
-        await buildApi.updateBuild(build, buildId);
-        console.log('Waiting for build to cancel...');
-        setTimeout(() => {
-          console.log('Wait timeout reached');
-          task.setResult(task.TaskResult.Failed, 'Timeout');
-        }, 10000);
+    const reason = task.getVariable('Build.Reason');
+    if (reason === 'IndividualCI' || reason === 'BatchedCI') {
+      console.log('Connecting to Build API...');
+      const buildApi = await getConnection().getBuildApi();
+      console.log('Getting build...');
+      const build = await buildApi.getBuild(buildId, project);
+      if (build.status === buildInterface.BuildStatus.NotStarted
+        || build.status === buildInterface.BuildStatus.InProgress) {
+        console.log(`Build ${buildInterface.BuildStatus.NotStarted ? 'not started' : 'in progress'}...`);
+        const options = {
+          cwd: sourcesDirectory,
+        };
+        console.log('Getting log for branch...');
+        const result = task.execSync('git', `diff-tree --no-commit-id --name-only -r ${commitId}`, options);
+        // check for errors
+        const files = result.stdout.toString().split('\n').filter(file => (file !== ''));
+        console.log('Getting definition for build...');
+        const definition = await buildApi.getDefinition(build.definition.id, project);
+        if (isInPathFilters(files, definition)) {
+          console.log('Files match! Continuing with build...');
+        } else {
+          console.log('Cancelling build...');
+          build.status = buildInterface.BuildStatus.Cancelling;
+          await buildApi.updateBuild(build, buildId);
+          console.log('Waiting for build to cancel...');
+          setTimeout(() => {
+            console.log('Wait timeout reached');
+            task.setResult(task.TaskResult.Failed, 'Timeout');
+          }, 10000);
+        }
       }
     }
     task.setResult(task.TaskResult.Succeeded, 'Success');
